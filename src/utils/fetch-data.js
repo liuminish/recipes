15d6f990378e9e4e4a02e981a605b787f6b0e1fe
Liu Min <liuminish@gmail.com>
2020-09-23 22:29:55 +0800

    Fix CORS error

--- src/utils/fetch-data.js
+++ src/utils/fetch-data.js
@@ -234,13 +234,13 @@ fetchData.uploadImage = image => {
   const formData = new FormData()
   formData.append('myImage', image)
 
-  const headers = new Headers
+  const headers = new Headers()
   headers.append('Access-Control-Allow-Origin', '*')
 
   const fetchOptions = {
     method: 'POST',
     body: formData,
-    headers
+    headers,
   };
 
   return fetch(url, fetchOptions).then(response => {
