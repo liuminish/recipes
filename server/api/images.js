1bf3c918ce999f33884b963ca6c0ab5e5717ccbf
Liu Min <liuminish@gmail.com>
2020-09-29 22:53:06 +0800

    changed image upload

--- server/api/images.js
+++ server/api/images.js
@@ -8,14 +8,13 @@ const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite'
 const fileupload = require('express-fileupload');
 imagesRouter.use(fileupload());
 
-const readChunk = require('read-chunk');
-const imageType = require('image-type');
+const FileType = require('file-type');
 
 // GET one image
 imagesRouter.get('/:name', (req, res, next) => {
 
     const options = {
-        root: path.join('/root/recipes/server/images/'),
+        root: path.resolve('images'),
         dotfiles: 'deny',
         headers: {
           'x-timestamp': Date.now(),
@@ -35,17 +34,36 @@ imagesRouter.get('/:name', (req, res, next) => {
 
 // POST/add one imagem
 imagesRouter.post('/', (req, res, next) => {
-  console.log('hiii')
-    
+  // console.log('hiii')
+  // const makeid = (length) => {
+  //   let result = '';
+  //   const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  //   const charactersLength = characters.length;
+  //   for ( var i = 0; i < length; i++ ) {
+  //      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  //   }
+  //   return result;
+  // }
+ 
+  // let fileType = '';
+
+  // async () => {
+  //   fileType = await FileType.fromFile(req.files.myImage)
+  // }
+
+  // console.log(fileType)
+
+  //   const fileName = makeid(10) + '.' + fileType;
+  //   console.log(fileName)
+
     const fileName = req.files.myImage.name;
-    console.log(fileName)
     const uploadPath = path.resolve('images', fileName);
 
     req.files.myImage.mv(uploadPath, (err) => {
         if (err) {
             next(err)
         } else {
-            res.status(201).json({path: fileName})
+            res.status(201).json({name: fileName})
         }
     })
 })
